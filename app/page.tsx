23c4a555297d9adac1eb95ac572cf286deb2c0fe
kisea-co <kisea@kiseaandco.com>
2026-08-19 16:26:23 -0400

    Reset Ajé Bumi homepage to editorial foundation

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { SacredFeminine } from "@/components/SacredFeminine";
 import { CollectionPreview } from "@/components/CollectionPreview";
-import { OceanMemory } from "@/components/OceanMemory";
-import { MaterialRitual } from "@/components/MaterialRitual";
-import { ClosingStatement } from "@/components/ClosingStatement";
 import { Newsletter } from "@/components/Newsletter";
 import { Footer } from "@/components/Footer";
 
@@ -15,9 +12,6 @@ export default function Home() {
       <Hero />
       <SacredFeminine />
       <CollectionPreview />
-      <OceanMemory />
-      <MaterialRitual />
-      <ClosingStatement />
       <Newsletter />
       <Footer />
     </main>
