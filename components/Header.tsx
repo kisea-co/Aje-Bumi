f4c4976427fc0885672b6dea06b77ac1ddb73450
kisea-co <kisea@kiseaandco.com>
2026-08-19 16:26:32 -0400

    Simplify Ajé Bumi navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,13 @@
 export function Header() {
   return (
     <header className="site-header">
-      <a className="wordmark" href="/" aria-label="Ajé Bumi home">
-        AJÉ BUMI
-      </a>
-
+      <a className="wordmark" href="/" aria-label="Ajé Bumi home">AJÉ BUMI</a>
       <nav className="nav" aria-label="Primary navigation">
-        <a href="/our-story">The House</a>
-        <a href="/#collection">Collections</a>
-        <a href="/#ritual">Materials</a>
-        <a href="/#private-access">Private Access</a>
+        <a href="/our-story">Our Story</a>
+        <a href="/#collection">Collection</a>
+        <a href="/#private-access">Journal</a>
       </nav>
-
-      <a className="header-note" href="/our-story">
-        Enter the house
-      </a>
+      <a className="header-note" href="/#collection">Shop</a>
     </header>
   );
 }
